0654ff6e1585c7399c2911c27c21ff6435a7132a
Violeta <violetagonzalezg@hotmail.com>
2017-05-10 18:08:11 +0200

    Añadido javascript y tablas.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,108 @@
+var elementos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 $.noConflict();
 jQuery(document).ready(function($) {
     // Code that uses jQuery's $ can follow here.
     $("#contactForm").on("submit",validarFormularioContacto);
+    $("#borrartodos").click(function (event) {
+        //attr ---> cambios de atributos
+        // prop --> propiedades
+        // is ----> validacion booleana
+        if($(this).is(":checked")){
+            $("tbody input[type=checkbox]").prop("checked",true);
+            //
+            //checked = checked
+            //selected= selected
+            //
+        }else{
+            $("tbody input[type=checkbox]").prop("checked",false);
+        }
 
+
+    });
 
     function validarFormularioContacto(){
         //recoger los valores de la vista
         var pdni = $("#dni").val();
+        var pnombre = $("#nombre").val();
+        var papellidos = $("#apellidos").val();
+        var ptelefono = $("#telefono").val();
+        var valido = false;
         //evaluarlos
-        var enviado= validarDni(pdni); //en funcion de si estan bien o mal o se envia o no
-        if(enviado){
-            $("#contactForm").submit();//se envia el Formulario(Consumir REST)
+        var dniValido= validarDni(pdni); //en funcion de si estan bien o mal o se envia o no
+        var nomValido = validarNombre(pnombre);
+        var apeValido = validarApellidos(papellidos);
+        var teleValido = validarTelefono(ptelefono);
+        $("#dni").siblings("div.text-error").text("");
+        $("#nombre").siblings("div.text-error").text("");
+        $("#apellidos").siblings("div.text-error").text("");
+        $("#telefono").siblings("div.text-error").text("");
+        if(dniValido&&nomValido&&apeValido&&teleValido){
+            // $("#contactForm").submit();//se envia el Formulario(Consumir REST)
+            valido = true;
         }else {
             //mostar mensaje de error
-            $("#dni").siblings("div.text-error").text("El DNI esta mal formado");
+            if(!dniValido){
+                $("#dni").siblings("div.text-error").text("El DNI esta mal formado");
+            }
+            if(!nomValido){
+                $("#nombre").siblings("div.text-error").text("El nombre tiene que tener al menos 3 letras");
+            }
+            if(!apeValido){
+                $("#apellidos").siblings("div.text-error").text("Los apellidos tienen que tener al menos 7 letras");
+            }
+            if(!teleValido){
+                $("#telefono").siblings("div.text-error").text("El telefono no es valido, tiene que tener 9 numeros");
+            }
             //text y html
         }
         return false;
     }
+    cargarArrayElementos();
+    function cargarArrayElementos() {
+        //recorrer el array
+        if (elementos.length > 0) {
+            for (var e in elementos) {
+                console.log(e);
+                var texto = "<tr><td><input type='checkbox' value='" + e + "'></td><td></td><td></td><td></td><td></td><td></td></tr>";
+                //añadir el html correspondiente a la página
+                $("#tablaElementos tbody").append(texto);
+                //-->
+            }
+            $("#tablaElementos tfoot td").html("<span class='text-error'>Total alumnos:"+alumnos.length,10+"</span>");
+        }else{
+            $("#tablaElementos").remove();
+            $("#listadoElementos").text("No se han encontrado elementos.")
+        }
+    }
 });
-
+function validarNombre(nombre){
+    const pattern = new RegExp(/[a-zA-Z]{3,}/);
+    return pattern.test(nombre);
+}
+function validarApellidos(apellidos) {
+    const pattern = new RegExp(/[a-zA-Z]{2,}\s[a-zA-Z]{2,}/);
+    return pattern.test(apellidos);
+}
+function validarTelefono(telefono){
+    var valido = true;
+    if(telefono!=""){
+        const pattern = new RegExp(/\d{9}/);
+        valido = pattern.test(telefono);
+    }
+    return valido ;
+}
 function validarDni(dni) {
-    var valido =true;
-    numero = parseInt(dni.substr(0,dni.length-1),10);
-    letr = dni.substr(dni.length-1,1);
-    numero = numero % 23;
-    letra='TRWAGMYFPDXBNJZSQVHLCKET';
-    letra=letra.substring(numero,numero+1);
-    if (letra!=letr.toUpperCase()) {
-        valido = false;
+    var valido =false;
+    const pattern = new RegExp(/\d{8}[A-Za-z]{1}/);
+    if(pattern.test(dni)){
+        numero = parseInt(dni.substr(0,dni.length-1),10);
+        letr = dni.substr(dni.length-1,1);
+        numero = numero % 23;
+        letra='TRWAGMYFPDXBNJZSQVHLCKET';
+        letra=letra.substring(numero,numero+1);
+        if (letra==letr.toUpperCase()) {
+            valido = true;
+        }
     }
     return valido;
 }
